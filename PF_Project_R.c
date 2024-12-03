0b978f991bdb1f2707bb5490380596886326fcfe
Ruhab <142657638+RuhabAyeshah@users.noreply.github.com>
2024-12-03 08:23:29 +0500

    books handling finished

--- PF_Project_R.c
+++ PF_Project_R.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Book{
+//Book Structure to hold an array of books. Each book has the below mentioned members. Available has a value of 0 or 1 with 1 meaning the book is available for issuing while 0 means its not.
+struct Book{ 
 	char name[150];
 	long long isbn;
 	char author[150];
@@ -11,6 +12,7 @@ struct Book{
 	int available;
 };
 
+//BookCount counts the number of books from the Book.txt file an makes sure that the books array is reallocated to hold the correct number of books. It also returns the number of books.
 int BookCount(struct Book **books){
 	int n;
 	char line[416];
@@ -28,6 +30,7 @@ int BookCount(struct Book **books){
 	return n;
 }
 
+//LoadBooks writes the data from books.txt into the books array. Each data item in the file is seperated by a tab to allow use of spaces in names (hence use of [^\t] which means to read until the tab occurs).
 void LoadBooks(struct Book *books, int num){
 	int i;
 	char line[416];
@@ -43,6 +46,7 @@ void LoadBooks(struct Book *books, int num){
 	fclose(fileptr);
 }
 
+//writes the book array to the books.txt file.
 void SaveBooks(struct Book *books, int n){
     int i;
     FILE *fileptr = fopen("Books.txt", "w");
@@ -57,7 +61,7 @@ void SaveBooks(struct Book *books, int n){
     fclose(fileptr);
 }
 
-
+//displays all the books currently in the array.
 void displayBooks(struct Book *books, int n){
 	int i;
 	
@@ -66,7 +70,26 @@ void displayBooks(struct Book *books, int n){
 	}
 }
 
+//adds a bok to the array and reallocates the array memory to hold one more space for the book. also increments the total count by 1 and makes sure the ISBN entered is 13 digits and unique.
 int AddBook(struct Book **books, int n){
+	long long isbntemp;
+	long long check = 1000000000000;
+    int i;
+	printf("Enter ISBN: ");
+	scanf("%lld",&isbntemp);
+	
+	if((isbntemp/check)>9||(isbntemp/check<1&&isbntemp!=check)){
+		printf("You must enter a valid 13 digit ISBN number.\n\n");
+		return n;
+	}
+	else{
+		for(i =0;i<n;i++){
+			if((*books)[i].isbn==isbntemp){
+				printf("That book already exists. Please check the ISBN number again.\n\n");
+				return n;
+			}	
+		}
+	}
     *books = (struct Book*)realloc(*books,(n+1)*sizeof(struct Book));
     
     if (*books == NULL) {
@@ -75,29 +98,26 @@ int AddBook(struct Book **books, int n){
     }
     fflush(stdin);
     printf("Enter book name: ");
-    fgets((*books)[n].name, 150, stdin);
+    fgets((*books)[n].name,150,stdin);
     (*books)[n].name[strcspn((*books)[n].name,"\n")]='\0';
     
     printf("Enter author name: ");
-    fgets((*books)[n].author, 150, stdin);
+    fgets((*books)[n].author,150,stdin);
     (*books)[n].author[strcspn((*books)[n].author,"\n")]='\0';
     
     printf("Enter publisher name: ");
-    fgets((*books)[n].publisher, 150, stdin);
+    fgets((*books)[n].publisher,150,stdin);
     (*books)[n].publisher[strcspn((*books)[n].publisher,"\n")]='\0';
-    
-	printf("Enter ISBN: ");
-	scanf("%lld",&(*books)[n].isbn);
     
     printf("Enter publishing year: ");
 	scanf("%d",&(*books)[n].year);
 	
+	(*books)[n].isbn = isbntemp;
 	(*books)[n].available = 1;
     return n+1;
 }
 
 int main(){
-	int i;
 	int numofBooks;
 	struct Book *books;
 	books = (struct Book*)malloc(20*sizeof(struct Book));
@@ -105,7 +125,6 @@ int main(){
 		printf("Memory Allocation Failed.");
 	}
 	numofBooks = BookCount(&books);
-	
 	LoadBooks(books, numofBooks);
 	displayBooks(books,numofBooks);
 	SaveBooks(books,numofBooks);
