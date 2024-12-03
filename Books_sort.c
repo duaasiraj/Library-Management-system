d483fa0939dd8f6421aa657e58f984e20b8c4776
Duaa Siraj <74562864+duaasiraj@users.noreply.github.com>
2024-12-03 20:13:38 +0500

    Sortingalgorithms_books

---
+++ Books_sort.c
@@ -0,0 +1,362 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+
+struct Book{ 
+	char name[150];
+	long long isbn;
+	char author[150];
+	char publisher[100];
+	int year;
+	int available;
+	char genre[100];
+};
+
+void Displaybooks(struct Book *books, int num);
+void sortNameAsc(struct Book *books, int num);
+void sortPublisherAsc(struct Book *books, int num);
+void sortYearAsc(struct Book *books, int num);
+void sortIsbnAsc(struct Book *books, int num);
+void sortAuthorAsc(struct Book *books, int num);
+void sortNameDsc(struct Book *books, int num);
+void sortPublisherDsc(struct Book *books, int num);
+void sortYearDsc(struct Book *books, int num);
+void sortIsbnDsc(struct Book *books, int num);
+void sortAuthorDsc(struct Book *books, int num);
+
+
+int main() {
+
+    struct Book books[20];
+
+    // Assign values to each book
+    strcpy(books[0].name, "The Catcher in the Rye");
+    books[0].isbn = 9780316769488;
+    strcpy(books[0].author, "J.D. Salinger");
+    strcpy(books[0].publisher, "Little, Brown and Company");
+    books[0].year = 1951;
+    books[0].available = 5;
+    strcpy(books[0].genre, "Fiction");
+
+    strcpy(books[1].name, "To Kill a Mockingbird");
+    books[1].isbn = 9780061120084;
+    strcpy(books[1].author, "Harper Lee");
+    strcpy(books[1].publisher, "J.B. Lippincott & Co.");
+    books[1].year = 1960;
+    books[1].available = 2;
+    strcpy(books[1].genre, "Fiction");
+
+    strcpy(books[2].name, "1984");
+    books[2].isbn = 9780451524935;
+    strcpy(books[2].author, "George Orwell");
+    strcpy(books[2].publisher, "Secker & Warburg");
+    books[2].year = 1949;
+    books[2].available = 4;
+    strcpy(books[2].genre, "Dystopian");
+
+    strcpy(books[3].name, "The Great Gatsby");
+    books[3].isbn = 9780743273565;
+    strcpy(books[3].author, "F. Scott Fitzgerald");
+    strcpy(books[3].publisher, "Charles Scribner's Sons");
+    books[3].year = 1925;
+    books[3].available = 3;
+    strcpy(books[3].genre, "Fiction");
+
+    strcpy(books[4].name, "Moby-Dick");
+    books[4].isbn = 9781503280786;
+    strcpy(books[4].author, "Herman Melville");
+    strcpy(books[4].publisher, "Harper & Brothers");
+    books[4].year = 1851;
+    books[4].available = 1;
+    strcpy(books[4].genre, "Adventure");
+
+    strcpy(books[5].name, "Pride and Prejudice");
+    books[5].isbn = 9781503290563;
+    strcpy(books[5].author, "Jane Austen");
+    strcpy(books[5].publisher, "T. Egerton");
+    books[5].year = 1813;
+    books[5].available = 6;
+    strcpy(books[5].genre, "Romance");
+
+    strcpy(books[6].name, "The Hobbit");
+    books[6].isbn = 9780261103344;
+    strcpy(books[6].author, "J.R.R. Tolkien");
+    strcpy(books[6].publisher, "George Allen & Unwin");
+    books[6].year = 1937;
+    books[6].available = 8;
+    strcpy(books[6].genre, "Fantasy");
+
+    strcpy(books[7].name, "The Lord of the Rings");
+    books[7].isbn = 9780261103573;
+    strcpy(books[7].author, "J.R.R. Tolkien");
+    strcpy(books[7].publisher, "George Allen & Unwin");
+    books[7].year = 1954;
+    books[7].available = 3;
+    strcpy(books[7].genre, "Fantasy");
+
+    strcpy(books[8].name, "The Chronicles of Narnia");
+    books[8].isbn = 9780064471193;
+    strcpy(books[8].author, "C.S. Lewis");
+    strcpy(books[8].publisher, "Geoffrey Bles");
+    books[8].year = 1950;
+    books[8].available = 4;
+    strcpy(books[8].genre, "Fantasy");
+
+    strcpy(books[9].name, "Brave New World");
+    books[9].isbn = 9780060850524;
+    strcpy(books[9].author, "Aldous Huxley");
+    strcpy(books[9].publisher, "Chatto & Windus");
+    books[9].year = 1932;
+    books[9].available = 5;
+    strcpy(books[9].genre, "Dystopian");
+
+    strcpy(books[10].name, "Crime and Punishment");
+    books[10].isbn = 9780486415871;
+    strcpy(books[10].author, "Fyodor Dostoevsky");
+    strcpy(books[10].publisher, "The Russian Messenger");
+    books[10].year = 1866;
+    books[10].available = 2;
+    strcpy(books[10].genre, "Psychological Fiction");
+
+    strcpy(books[11].name, "War and Peace");
+    books[11].isbn = 9781400079988;
+    strcpy(books[11].author, "Leo Tolstoy");
+    strcpy(books[11].publisher, "The Russian Messenger");
+    books[11].year = 1869;
+    books[11].available = 3;
+    strcpy(books[11].genre, "Historical Fiction");
+
+    strcpy(books[12].name, "The Odyssey");
+    books[12].isbn = 9780143039952;
+    strcpy(books[12].author, "Homer");
+    strcpy(books[12].publisher, "Penguin Classics");
+    books[12].year = -800;
+    books[12].available = 5;
+    strcpy(books[12].genre, "Epic Poetry");
+
+    strcpy(books[13].name, "Frankenstein");
+    books[13].isbn = 9780486282114;
+    strcpy(books[13].author, "Mary Shelley");
+    strcpy(books[13].publisher, "Lackington, Hughes, Harding, Mavor & Jones");
+    books[13].year = 1818;
+    books[13].available = 7;
+    strcpy(books[13].genre, "Science Fiction");
+
+    strcpy(books[14].name, "Dracula");
+    books[14].isbn = 9780141439846;
+    strcpy(books[14].author, "Bram Stoker");
+    strcpy(books[14].publisher, "Archibald Constable and Company");
+    books[14].year = 1897;
+    books[14].available = 4;
+    strcpy(books[14].genre, "Horror");
+
+    strcpy(books[15].name, "Wuthering Heights");
+    books[15].isbn = 9780141439556;
+    strcpy(books[15].author, "Emily Bronte");
+    strcpy(books[15].publisher, "Thomas Cautley Newby");
+    books[15].year = 1847;
+    books[15].available = 6;
+    strcpy(books[15].genre, "Romance");
+
+    strcpy(books[16].name, "The Picture of Dorian Gray");
+    books[16].isbn = 9780141439570;
+    strcpy(books[16].author, "Oscar Wilde");
+    strcpy(books[16].publisher, "Lippincott's Monthly Magazine");
+    books[16].year = 1890;
+    books[16].available = 4;
+    strcpy(books[16].genre, "Gothic Fiction");
+
+    strcpy(books[17].name, "Les Miserables");
+    books[17].isbn = 9780451419439;
+    strcpy(books[17].author, "Victor Hugo");
+    strcpy(books[17].publisher, "A. Lacroix, Verboeckhoven & Cie");
+    books[17].year = 1862;
+    books[17].available = 5;
+    strcpy(books[17].genre, "Historical Fiction");
+
+    strcpy(books[18].name, "The Divine Comedy");
+    books[18].isbn = 9780140448955;
+    strcpy(books[18].author, "Dante Alighieri");
+    strcpy(books[18].publisher, "Felice Le Monnier");
+    books[18].year = 1320;
+    books[18].available = 8;
+    strcpy(books[18].genre, "Epic Poetry");
+
+    strcpy(books[19].name, "The Canterbury Tales");
+    books[19].isbn = 9780140422344;
+    strcpy(books[19].author, "Geoffrey Chaucer");
+    strcpy(books[19].publisher, "William Caxton");
+    books[19].year = 1400;
+    books[19].available = 6;
+    strcpy(books[19].genre, "Poetry");
+    int num_books = 20;
+    printf("=====BY NAME=====\n");
+	sortNameDsc(books,20);
+	printf("\n\n============BY AUTHOR==========\n");
+		sortAuthorDsc(books,20);
+	
+	printf("\n\n============BY Isbn==========\n");
+		sortIsbnDsc(books,20);
+	printf("\n\n============BY Year==========\n");
+		sortYearDsc(books,20);
+		printf("\n\n============BY pub==========\n");
+	sortPublisherDsc(books,20);
+    return 0;
+}
+
+//Ascending functions definition-bubble sort
+void Displaybooks(struct Book *books,int num){
+	for (int i=0;i<num;i++){
+		printf("%-35s %15lld\t %-25s %4d %-35s %2d\n",books[i].name,books[i].isbn,books[i].author,books[i].year,books[i].publisher,books[i].available);
+	}
+}
+
+void sortNameAsc(struct Book *books,int num){
+    struct Book temp;
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = 0; j < num - 1; j++) {
+            if (strcmp(books[j].name, books[j + 1].name) > 0) {
+                temp = books[j];
+                books[j] = books[j + 1];
+                books[j + 1] = temp;
+            }
+        }
+    }
+    Displaybooks(books,num);
+}
+
+
+void sortPublisherAsc(struct Book *books,int num){
+    struct Book temp;
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = 0; j < num  - 1; j++) {
+            if (strcmp(books[j].publisher, books[j + 1].publisher) > 0) {
+                temp = books[j];
+                books[j] = books[j + 1];
+                books[j + 1] = temp;
+            }
+        }
+    }
+    Displaybooks(books,num);
+}
+
+void sortYearAsc(struct Book *books,int num){
+    struct Book temp;
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = 0; j < num - 1; j++) {
+            if (books[j].year> books[j + 1].year) {
+                temp = books[j];
+                books[j] = books[j + 1];
+                books[j + 1] = temp;
+            }
+        }
+    }
+    Displaybooks(books,num);
+}
+
+void sortIsbnAsc(struct Book *books,int num){
+    struct Book temp;
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = 0; j < num - 1; j++) {
+            if (books[j].isbn> books[j + 1].isbn) {
+                temp = books[j];
+                books[j] = books[j + 1];
+                books[j + 1] = temp;
+            }
+        }
+    }
+    Displaybooks(books,num);
+}
+
+void sortAuthorAsc(struct Book *books,int num){
+    struct Book temp;
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = 0; j < num  - 1; j++) {
+            if (strcmp(books[j].author, books[j + 1].author) > 0) {
+                temp = books[j];
+                books[j] = books[j + 1];
+                books[j + 1] = temp;
+            }
+        }
+    }
+    Displaybooks(books,num);
+}
+
+
+//descending
+void sortNameDsc(struct Book *books,int num){
+    struct Book temp;
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = 0; j < num - 1; j++) {
+            if (strcmp(books[j].name, books[j + 1].name) < 0) {
+                temp = books[j];
+                books[j] = books[j + 1];
+                books[j + 1] = temp;
+            }
+        }
+    }
+    Displaybooks(books,num);
+}
+
+
+void sortPublisherDsc(struct Book *books,int num){
+    struct Book temp;
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = 0; j < num  - 1; j++) {
+            if (strcmp(books[j].publisher, books[j + 1].publisher) < 0) {
+                temp = books[j];
+                books[j] = books[j + 1];
+                books[j + 1] = temp;
+            }
+        }
+    }
+    Displaybooks(books,num);
+}
+
+void sortYearDsc(struct Book *books,int num){
+    struct Book temp;
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = 0; j < num - 1; j++) {
+            if (books[j].year< books[j + 1].year) {
+                temp = books[j];
+                books[j] = books[j + 1];
+                books[j + 1] = temp;
+            }
+        }
+    }
+    Displaybooks(books,num);
+}
+
+void sortIsbnDsc(struct Book *books,int num){
+    struct Book temp;
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = 0; j < num - 1; j++) {
+            if (books[j].isbn< books[j + 1].isbn) {
+                temp = books[j];
+                books[j] = books[j + 1];
+                books[j + 1] = temp;
+            }
+        }
+    }
+    Displaybooks(books,num);
+}
+
+void sortAuthorDsc(struct Book *books,int num){
+    struct Book temp;
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = 0; j < num  - 1; j++) {
+            if (strcmp(books[j].author, books[j + 1].author) < 0) {
+                temp = books[j];
+                books[j] = books[j + 1];
+                books[j + 1] = temp;
+            }
+        }
+    }
+    Displaybooks(books,num);
+}
+
+
